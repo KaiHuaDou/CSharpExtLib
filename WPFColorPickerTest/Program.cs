aff346764743b97783544b9bceba15f911663e69
KaiHuaDou <KaiHuaDou@FkqSoft.com>
2021-01-28 16:37:49 +0800

    增加了颜色选择器测试

---
+++ WPFColorPickerTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using CSharpExtLibrary;
+
+namespace WPFColorPickerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WPFColorPicker.NameColor nameColor = WPFColorPicker.PickColor( );
+            System.Console.WriteLine(nameColor.Name);
+            System.Console.WriteLine(nameColor.color.A);
+            System.Console.WriteLine(nameColor.color.R);
+            System.Console.WriteLine(nameColor.color.G);
+            System.Console.WriteLine(nameColor.color.B);
+            System.Console.ReadLine( );
+        }
+    }
+}
