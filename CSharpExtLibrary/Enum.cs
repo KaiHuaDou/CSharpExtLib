a6278e8ad6c2a519b36afb050672579beac92ce6
KaiHuaDou <KaiHuaDou@FkqSoft.com>
2021-01-28 14:05:16 +0800

    添加枚举类型文件

---
+++ CSharpExtLibrary/Enum.cs
@@ -0,0 +1,22 @@
+﻿namespace CSharpExtLibrary
+{
+    public enum Possibility
+    {
+        Impossible = 0,
+        OneOfTenpossible = 10,
+        QuarterPossible = 25,
+        Possible = 50,
+        TripleQuarter = 75,
+        Certain = 100
+    }
+    public enum PublicLevel
+    {
+        Sealed = 1,
+        Private = 2,
+        Internal = 3,
+        Protected = 4,
+        Public = 5,
+        Abstract = 6,
+        Everyone = int.MaxValue
+    }
+}
