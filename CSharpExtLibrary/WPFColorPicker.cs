42ee5e8d286ea8d3dd26a24a9399579d0d6bc51e
KaiHuaDou <KaiHuaDou@FkqSoft.com>
2021-01-28 14:05:33 +0800

    添加WPFColorPicker类

---
+++ CSharpExtLibrary/WPFColorPicker.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Media;
+using System.Windows.Forms;
+namespace CSharpExtLibrary
+{
+    static class WPFColorPicker
+    {
+        static Color PickColor()
+        {
+            Color color = new Color( );
+            ColorDialog cd = new ColorDialog( );
+            cd.AnyColor = true;
+            cd.FullOpen = true;
+            cd.SolidColorOnly = false;
+            if(cd.ShowDialog( ) == DialogResult.OK)
+            {
+                color.A = cd.Color.A;
+                color.R = cd.Color.R;
+                color.G = cd.Color.G;
+                color.B = cd.Color.B;
+            }
+            return color;
+        }
+    }
+}
