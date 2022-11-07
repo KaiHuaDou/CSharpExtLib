c002b288cc7076ceb8a99fc8b8e92b871b6aca2b
KaiHuaDou <74781268+KaiHuaDou@users.noreply.github.com>
2022-11-07 15:34:31 +0800

    添加README和正片叠底

--- CSharpExtLib/Easy/EasyColor.cs
+++ CSharpExtLib/Easy/EasyColor.cs
@@ -1,16 +1,35 @@
 ﻿using System.Windows.Media;
+using System;
 
 namespace CSharpExtLibrary.Easy
 {
     public static class EasyColor
     {
-        public static Color Mix(Color color1, Color color2)
+        public static Color Normal(Color up, Color down)
         {
             Color result = new Color();
-            result.R = (byte)((color1.R + color2.R) / 2);
-            result.G = (byte)((color1.G + color2.G) / 2);
-            result.B = (byte)((color1.B + color2.B) / 2);
-            result.A = (byte)((color1.A + color2.A) / 2);
+            result.R = (byte)((up.R + down.R) / 2);
+            result.G = (byte)((up.G + down.G) / 2);
+            result.B = (byte)((up.B + down.B) / 2);
+            result.A = (byte)((up.A + down.A) / 2);
+            return result;
+        }
+        public static Color Multiply(Color up, Color down)
+        {
+            Color result = new Color();
+            result.R = (byte)(up.R * down.R / 255);
+            result.G = (byte)(up.G * down.G / 255);
+            result.B = (byte)(up.B * down.B / 255);
+            result.A = (byte)(up.A * down.A / 255);
+            return result;
+        }
+        public static Color Min(Color up, Color down)
+        {
+            Color result = new Color();
+            result.R = (byte) Math.Abs(up.R - down.R);
+            result.G = (byte) Math.Abs(up.G - down.G);
+            result.B = (byte) Math.Abs(up.B - down.B);
+            result.A = (byte) Math.Abs(up.A - down.A);
             return result;
         }
     }
---
+++ README.md
@@ -0,0 +1,21 @@
+# CSharpExtLib
+一些十分有用的C#代码段
+> Author: KaiHuaDou
+
+# 目前功能
+
+## CSharpExtLib
++ FontManager：一键判断字体类型是否为符号字体（如Wingdings）
++ JumpChar：控制台实现文本“打字机”效果
+
+## CSharpExtLib.Easy
++ EasyColor：模拟Photoshop的各种颜色混合模式的实用类。
++ EasyConfig：使用XML序列化对象（通常是设置）的实用类。
++ EasyConverter：转换WinForm和WPF数据结构的静态实用类
+
+## CSharpExtLib.Math
++ Fraction：进行分数运算的实用类
++ StdApi：其他数学运算
+
+## CSharpExtLib.WPF
++ 敬请期待……
